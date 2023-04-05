71d0fa221948ae566138f15671b5825e85a3eb7d
Henok Aklilu <116839985+henok-aklilu@users.noreply.github.com>
2023-04-05 19:06:40 +0300

    Update 0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
 """ 0-rectangel : module
+contains a class defination and more related to rectangles.
+Class:
+    Rectangle: used to define a Rectangle object.
 """
 
 
 class Rectangle:
-    """ Sekeleton for Rectangle Class
     """
+    This module contains a definations of A rectangle objects
+    and methods to act upon them.
+    Methods:
+        No methods yet.
+    Attributes:
+        No attributes yet.
+    """
+    pass
     
